2fccfbeda825c45117f2d67c4f1bdc2813bda1f2
Oktay Horasan <oktayhorasan@gmail.com>
2024-12-14 23:56:48 +0300

    goit-js-hw-06

---
+++ js/task-2.js
@@ -0,0 +1,28 @@
+console.log("%cTask 2", "color: #12ff00; font-weight: bold;");
+
+class Storage {
+  #items;
+
+  constructor(initialItems) {
+    this.#items = initialItems;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter((item) => item !== itemToRemove);
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
